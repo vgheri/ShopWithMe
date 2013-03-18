95674c9e9d58ecab1a1a62dfefe2ddde78ecc75f
Valerio Gheri <vashds2001@yahoo.it>
2013-03-18 12:25:29 -0400

    Fixed put route and completed AccountHandler use cases

--- Routes.js
+++ Routes.js
@@ -8,7 +8,7 @@
  function setup(app, handlers) {
     app.post('/profiles', handlers.account.createAccount);
     app.get('/profiles/:username', handlers.account.getAccount);
-    app.put('/profiles', handlers.account.updateAccount);
+    app.put('/profiles/:username', handlers.account.updateAccount);
     app.delete('/profiles/:username', handlers.account.deleteAccount);
 }
 
--- handlers/AccountHandler.js
+++ handlers/AccountHandler.js
@@ -26,10 +26,13 @@
          if (err) {             
             winston.log('error', 'An error has occurred while processing a request to create an ' +
             'account from ' + req.connection.remoteAddress + 
-            '. Stack trace: ' + err.stack);            
+            '. Timestamp: ' + new Date() + '. Stack trace: ' + err.stack);            
             res.jsonp(400, {error: err.message});
          }
-         else {                         
+         else {        
+            winston.log('info', 'Account ' + username + ' has been created.' + 
+                'Request from address ' + req.connection.remoteAddress + '.' +
+                'Timestamp: ' + new Date()); 
             res.jsonp(201, account);
          }         
      });
@@ -41,28 +44,109 @@
    var username = req.params.username;
    findAccountByUsername(username, function(err, account) {
      if (err) {
-       winston.log('error', 'An error has occurred while processing a request to retrieve an ' +
-            'account from ' + req.connection.remoteAddress + 
-            '. Stack trace: ' + err.stack);
-       res.jsonp(400, {error: err.message});
+         winston.log('error', 'An error has occurred while processing a request to retrieve ' +
+            'account ' + username + ' from ' + req.connection.remoteAddress + 
+            '. Timestamp: ' + new Date() + '. Stack trace: ' + err.stack);
+         res.jsonp(400, {error: err.message});
      }
      else {
        if (account) {
+         winston.log('info', 'Account ' + username + ' has been retrieved.' + 
+                'Request from address ' + req.connection.remoteAddress + '.' +
+                'Timestamp: ' + new Date());  
          res.jsonp(200, account);
        }
        else {
+         winston.log('info', 'Could not retrieve account ' + username + ', no ' + 
+                'such username. Request from address ' + req.connection.remoteAddress + '.' +
+                'Timestamp: ' + new Date());  
          res.jsonp(404, {error: "No account found matching " + username});
        }
      }
    });
  }
  
- function handleUpdateAccountRequest(firstName, lastName, request, response) {
+ function handleUpdateAccountRequest(req, res) {
+     // Retrieve the username from the request
+     var username = req.params.username;
+     var updatedAccount = req.body; 
+     updatedAccount.username = username;
      
+     updateAccount(updatedAccount, function(err, account) {
+         if (err) {
+             winston.log('error', 'An error has occurred while processing a request to update ' +
+             'account ' + username + ' from ' + req.connection.remoteAddress + 
+             '. Timestamp: ' + new Date() + '. Stack trace: ' + err.stack);
+             res.jsonp(400, {error: err.message});
+         } 
+         else {
+             if (account) {
+                 winston.log('info', 'Account ' + username + ' has been updated.' + 
+                 'Request from address ' + req.connection.remoteAddress + '.' +
+                 'Timestamp: ' + new Date()); 
+                 res.jsonp(200, account);              
+             }
+             else {
+                 winston.log('info', 'Could not update account ' + username + ', no ' + 
+                 'such username. Request from address ' + req.connection.remoteAddress + '.' +
+                 'Timestamp: ' + new Date());  
+                 res.jsonp(404, {error: "No account found matching " + username});             
+             }
+         }
+     });
+     
+     /* Old approach
+     // Retrieve the account associated with this username
+     findAccountByUsername(username, function(err, account) {
+        if (err) {
+            winston.log('error', 'An error has occurred while processing a request to update ' +
+            'account ' + username + ' from ' + req.connection.remoteAddress + 
+            '. Stack trace: ' + err.stack);
+            res.jsonp(400, {error: err.message});
+        }
+        else {
+           if (account) {
+             // Update the account with the info modified by the user
+             if (account.firstName !== updatedAccount.firstName) {
+                 account.firstName = updatedAccount.firstName;                 
+             }
+             if (account.lastName !== updatedAccount.lastName) {
+                 account.lastName = updatedAccount.lastName;
+             }
+             
+           }
+           else {
+             res.jsonp(404, {error: "No account found matching " + username});
+           }
+        }
+     });
+     */
  }
  
- function handleDeleteAccountRequest(username, request, response) {
-     
+ function handleDeleteAccountRequest(username, req, res) {          
+     disableAccount(username, function(err, account) {
+         if (err) {
+             winston.log('error', 'An error has occurred while processing a request to disable ' +
+             'account ' + username + ' from ' + req.connection.remoteAddress + 
+             '. Timestamp: ' + new Date() + '. Stack trace: ' + err.stack);
+             res.jsonp(400, {error: err.message});
+         } 
+         else {
+             if (account) {
+                 winston.log('info', 'Account ' + username + ' has been disabled.' + 
+                 'Request from address ' + req.connection.remoteAddress + '.' +
+                 'Timestamp: ' + new Date());
+                 // No need to return anything. We just disabled the account 
+                 res.jsonp(204);                 
+             }
+             else {
+                 winston.log('info', 'Could not disable account ' + username + ', no ' + 
+                 'such username. Request from address ' + req.connection.remoteAddress + '.' +
+                 'Timestamp: ' + new Date()); 
+                 res.jsonp(404, {error: "No account found matching " + username});             
+             }
+         }
+     });
  } 
  
    
@@ -85,12 +169,16 @@
    });
  }
  
- function updateAccount(account, callback) {
-      throw new Error("Not implemented");
+ function updateAccount(account, callback) {     
+     var query = { username: account.username };
+     var options = {new: true};
+     accountModule.Account.findOneAndUpdate(query, {firstName: account.firstName, lastName: account.lastName}, options, callback);
  }
  
  function disableAccount(username, callback) {
-     throw new Error("Not implemented");
+     var query = { username: username };
+     var options = {new: true};
+     accountModule.Account.findOneAndUpdate(query, {isActive: false, canLogin: false}, options, callback);
  }
 
  exports.AccountHandler = new AccountHandler();
